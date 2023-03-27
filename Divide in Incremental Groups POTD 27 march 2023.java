a2d2bffe386a6d9c55d6fd7776a79d304cfd5f31
Ashutosh Kumar <84832754+ashu473@users.noreply.github.com>
2023-03-27 22:56:37 +0530

    Add files via upload

---
+++ Divide in Incremental Groups POTD 27 march 2023.java
@@ -0,0 +1,39 @@
+class Solution {
+    int mod = 1000000007; 
+    int dp[][][];
+    public int countWaystoDivide(int n, int k) {
+        // Code here
+        dp = new int[n+1][k+1][n+1];
+       for(int x[][]:dp)
+       {
+           for(int y[] :x)
+           Arrays.fill(y,-1);
+       }
+       
+          return solve(n, k, n);
+    }
+    
+     int solve(int n, int k, int last)
+    {
+    	// base case
+    	if (k == 0)
+    	{
+    		return (n==0)? 1:0;
+    	}
+    	
+    	if (dp[n][k][last] != -1)
+    	{
+    		return dp[n][k][last];
+    	}
+    	int answer = 0;
+    
+    	// try all possible sizes of Kth group
+    	for (int s = 1; s <= Math.min(n, last); s++)
+    	{
+    		answer += solve(n - s, k - 1, s);
+    		answer = answer % mod;
+    	}
+    
+    	return dp[n][k][last] = answer;
+    }
+}
