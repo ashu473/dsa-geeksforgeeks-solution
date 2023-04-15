c7138a679f4a35946c34b61175ad771dc2638e06
Ashutosh Kumar <84832754+ashu473@users.noreply.github.com>
2023-04-15 23:27:18 +0530

    Add files via upload

---
+++ Find Total Time Taken POTD 15 April 2023.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            
+            int n=Integer.parseInt(in.readLine().trim());
+            int a[]=new int[n];
+            String s[]=in.readLine().trim().split(" ");
+            for(int i=0;i<n;i++){
+                a[i]=Integer.parseInt(s[i]);
+            }
+            s=in.readLine().trim().split(" ");
+            int time[]=new int[n];
+            for(int i=0;i<n;i++){
+                time[i]=Integer.parseInt(s[i]);
+            }
+            Solution ob=new Solution();
+            long ans=ob.totalTime(n,a,time);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static long totalTime(int n,int arr[],int time[])
+    {
+        long count=-1;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            if(map.containsKey(arr[i]))
+            {
+                map.put(arr[i],map.get(arr[i])+1);
+                count+=time[arr[i]-1];
+            }
+            else
+            {
+                map.put(arr[i],1);
+                count++;
+            }
+        }
+        return count;
+    }
+}
